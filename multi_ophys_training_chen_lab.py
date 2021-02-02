f1d1b0bcbdc245b9af3ac46087180097cffd8bac
Danielle LaMay <lamay.d@northeastern.edu>
2021-02-02 00:29:29 -0500

    Update multi_ophys_training_chen_lab.py

--- multi_ophys_training_chen_lab.py
+++ multi_ophys_training_chen_lab.py
@@ -31,7 +31,8 @@ generator_test_param["steps_per_epoch"] = steps_per_epoch
 generator_test_param["randomize"] = 1
 
 local_train_path = '/net/claustrum2/mnt/data/Projects/Perirhinal/Animals/pr012/2P/pr012-1/PreProcess/A0_Ch0'
-train_paths = os.listdir(local_train_path)
+#train_paths = os.listdir(local_train_path)
+train_paths = [f for f in os.listdir(local_train_path) if os.path.isfile(os.path.join(local_train_path, f))]
 
 generator_param_list = []
 for indiv_path in train_paths:
