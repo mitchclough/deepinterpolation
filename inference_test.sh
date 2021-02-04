09351e6fba28c1fe0f8683aeb4c8e7f645e75e18
Danielle LaMay <lamay.d@northeastern.edu>
2021-02-04 15:59:02 -0500

    Create inference_test.sh

---
+++ inference_test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+
+#this requests a node with a certain number of cpu cores
+#$ -pe omp 16
+
+
+
+#this makes it so you'll get an email at the (b)eginning of the job, (e)nd of the job, and on an (a)bort of the job
+#$ -m bea
+
+#this merges output and error files into one file
+#$ -j y
+
+#this sets the project for the script to be run under
+#$ -P jchenlab
+
+module load python3
+pip install --user s3fs
+module load tensorflow/2.1.0
+
+
+cd /usr3/bustaff/dlamay/deepinterpolation/examples/
+
+
+python example_tiny_inference.py
