9bec5b053e93bb0f672f3a359a9dcf8a5dbf9cad
Danielle LaMay <lamay.d@northeastern.edu>
2021-03-15 15:07:50 -0400

    Update multi_ophys_training_chen_lab_transfer.py

--- multi_ophys_training_chen_lab_transfer.py
+++ multi_ophys_training_chen_lab_transfer.py
@@ -65,8 +65,8 @@ for i in range(len(train_paths)):
     generator_param["pre_post_frame"] = 30
     generator_param["train_path"] = train_paths[i]
     generator_param["batch_size"] = 5
-    generator_param["start_frame"] = frame_nums[i]
-    generator_param["end_frame"] = frame_nums[i]+1
+    generator_param["start_frame"] = (frame_nums[i])-5
+    generator_param["end_frame"] = frame_nums[i]
     generator_param["steps_per_epoch"] = steps_per_epoch
     generator_param["randomize"] = 1
     generator_param["pre_post_omission"] = 1
