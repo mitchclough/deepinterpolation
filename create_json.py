8d568c06ed05fc8cfef589763591bb84ecdd201f
Danielle LaMay <48498276+dlamay@users.noreply.github.com>
2021-06-30 15:09:33 -0400

    Update create_json.py

--- create_json.py
+++ create_json.py
@@ -10,6 +10,8 @@ animal = 'pr012'
 #change to animal path
 animal_path ='/net/claustrum2/mnt/data/Projects/Perirhinal/Animals/' + animal + '/2P/'
 local_train_paths = glob.glob(os.path.join(animal_path, animal +'-*/PreProcess/*_Ch0'))
+#change to what you want json file to be named
+output_name = "pr012_files.json"
 
 train_paths_td = []
 for local_train_path in local_train_paths:
@@ -23,5 +25,5 @@ print(len(train_paths_td))
 json_obj = json.dumps(train_paths_td)
 
 #change to name json file
-with open("pr012_files.json", "w") as outfile:
+with open(output_name, "w") as outfile:
     outfile.write(json_obj)
